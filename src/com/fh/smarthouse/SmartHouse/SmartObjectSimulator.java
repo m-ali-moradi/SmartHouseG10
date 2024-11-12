7c627010390e6a51a80ecef41e1d5f8925eb4a98
bhava <bhava@172.22.207.113>
2024-11-12 13:25:51 +0100

    Created a new project

---
+++ src/com/fh/smarthouse/SmartHouse/SmartObjectSimulator.java
@@ -0,0 +1,10 @@
+package com.fh.smarthouse.SmartHouse;
+
+public class SmartObjectSimulator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module SmartHouseG10 {
+}
